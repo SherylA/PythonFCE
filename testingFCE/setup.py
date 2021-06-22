1a0204ce33f904917c9a300538f134f303ba6e47
unknown <sheryl.avendano@udea.edu.co.com>
2021-06-21 23:10:21 -0500

    Testing name change

--- testingFCE/setup.py
+++ testingFCE/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='testpy1',
+    name='testingFCE',
     version='0.1.0',    
     description='Testing Python package',
     url='https://github.com/SherylA/testingFCE',
     author='Sheryl Avenaño',
     author_email='sheryl.avendano@udea.edu.co',
     license='BSD 2-clause',
-    packages=['testpy1'],
+    packages=['testingFCE'],
     install_requires=['numpy', 'pandas'           
                       ],
 
